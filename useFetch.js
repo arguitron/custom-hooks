3d79f2779ffbaf73369b2c84bcdcc1ccb015a89f
Abel Guitron <guitronaca@gmail.com>
2022-01-25 20:12:32 -0600

    :tada: First hooks.
    useCounter, useFetch adn useForm

---
+++ useFetch.js
@@ -0,0 +1,38 @@
+import { useRef, useState } from "react";
+import { useEffect } from "react/cjs/react.development";
+
+export const useFetch = (url) => {
+  const isMounted = useRef(true);
+  const [state, setState] = useState({
+    data: null,
+    loading: true,
+    error: null,
+  });
+  useEffect(() => {
+    return () => {
+      isMounted.current = false;
+    };
+  }, []);
+  useEffect(() => {
+    setState({
+      data: null,
+      loading: true,
+      error: null,
+    });
+    fetch(url)
+      .then((resp) => resp.json())
+      .then((data) => {
+        setTimeout(() => {
+          if (isMounted.current) {
+            setState({
+              loading: false,
+              error: null,
+              data: data,
+            });
+          }
+        }, 4000);
+      });
+  }, [url]);
+
+  return state;
+};
